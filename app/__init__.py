4265e13e5189a5cef93fc49cded120070669ef04
aoa9 <aoa9@cornell.edu>
2022-09-09 15:10:08 -0400

    db and models set up

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.routes.users import users
 from app.routes.tasks import tasks
 import os
@@ -10,7 +12,13 @@ app =  Flask(__name__)
 app.register_blueprint(users, url_prefix='/users')
 app.register_blueprint(tasks, url_prefix='/tasks')
 
-
+# Databse config
+basedir = os.path.dirname(os.path.abspath(__file__))
+db_file = "sqlite:///{}".format(os.path.join(basedir, "app.db")) 
+app.config["SQLALCHEMY_DATABASE_URI"] = db_file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.secret_key = os.getenv('somerandomstring')
 
